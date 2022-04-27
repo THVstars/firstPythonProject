924832907ef954adb405dc792ea961a116c92806
THVstars <kcarolinasanchez@gmail.com>
2022-04-26 21:19:57 -0400

    Updated employee_checker.

--- employee_checker.py
+++ employee_checker.py
@@ -1,28 +1,31 @@
 import re
 
-COMPANY_NAME = "Tech Inc."
+COMPANY_NAME = "Vante Inc." # changed the name to one i like.
 
-EMPLOYEES = ["Alice", "Bob", "Cameron", "Dan", "Ellen", "Frank", "Grace", "Harry"]
+EMPLOYEES = ["Taehyung", "Yeontan", "Nyubella", "Carolina", "Rosario", "Victoria", "John", "Snowball"] # changed the names to ones i like.
 
 # Welcome message + employee names listed out
-print("\nWelcome to the "+COMPANY_NAME+" employee check-in\n")
+print("\nWelcome to the " + COMPANY_NAME + " employee check-in\n")
 print("We at " + COMPANY_NAME + " are very proud of our employees: ")
 for emp_name in EMPLOYEES:
-    print("-- "+emp_name)
+    print("-- " + emp_name)
 print("\n")
 
 # user input section (buggy code)
 
 accept = input("Do you work for " + COMPANY_NAME + "?\n(yes/no): ")
 
-if re.search("y", accept[0]) is not None: # bonus: checks if the input starts with y and accepts it if it does
+while accept[0].casefold() == "" or accept[0].casefold() != "y" and accept[0].casefold() != "n":
+    accept = input("Do you work for " + COMPANY_NAME + "?\n(yes/no): ")  # bonus: keeps asking this question until an acceptable answer is inputted. acceptable answers begin with y or n, so if the input does not begin with either or is empty, it keeps asking.
+
+if accept[0].casefold().replace(" ", "") == "y":  # bonus: checks if the input starts with y and accepts it if it does.
     name = input("What is your name?\nName: ")
     for emp_name in EMPLOYEES:
-        if name.casefold().replace(" ", "") == emp_name.casefold(): # bonus: case-insensitive and ignores empty spaces
+        if name.casefold().replace(" ", "") == emp_name.casefold():  # bonus: case-insensitive and ignores empty spaces.
             print("Thank you " + emp_name + ", you are now checked in.")
             break
-    else: # only prints out once, after checking every name
+    else:  # only prints out once, after checking every name.
         print("You're not an employee")
-elif accept.casefold() == "no": # bonus: case-insensitive
+elif accept[0].casefold().replace(" ", "") == "n":  # bonus: case-insensitive.
     print("This service is not for you. Exiting program...")
     exit()
